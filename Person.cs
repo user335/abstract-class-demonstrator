181a28929b0f619d34601a35f6e1c25405ae029f
Andy Weekes <userthreethreefive@gmail.com>
2019-02-15 14:37:53 -0700

    First commit

---
+++ Person.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public abstract class Person
+    {
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+
+        public abstract void SayName();
+
+        static void Main()
+        {
+            var employee = new Employee();
+            employee.firstName = "Sample";
+            employee.lastName = "Student";
+            employee.SayName();
+            Console.ReadKey();
+        }
+    }
+}
